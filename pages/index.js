932a31963917f0bb1937ebaf960591f498d4caf6
thisoldbear <danielwilliams.design@gmail.com>
2021-07-26 17:48:32 +0100

    Remove Netlify CMS widget

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,11 @@
-import Head from "next/head";
 import { Component } from "react";
 import { attributes, react as HomeContent } from "../content/home.md";
-import Script from "next/script";
 
 export default class Home extends Component {
   render() {
     let { title, cats } = attributes;
     return (
       <>
-        <Head>
-          <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
-        </Head>
         <article>
           <h1>{title}</h1>
           <HomeContent />
